031ca238ab1d2bb199032e452a41cc971ca985e5
Braylin20 <braylin_vasquez@ucne.edu.do>
2025-01-05 15:45:51 -0400

    Add an array to save the projects

---
+++ portfolio-michelle/src/data/Projects.js
@@ -0,0 +1,22 @@
+
+
+export const dataProjects =  [
+    {
+        'projectName': 'OvreBuilding',
+        'description': 'The integration of balconies in the architectural composition of a\n' +
+            'residential building is always a great challenge',
+        'imgName': 'OvreBuilding.png',
+    },
+    {
+        'projectName': 'OvreBuilding',
+        'description': 'The integration of balconies in the architectural composition of a\n' +
+            'residential building is always a great challenge',
+        'imgName': 'OvreBuilding.png',
+    },
+    {
+        'projectName': 'OvreBuilding',
+        'description': 'The integration of balconies in the architectural composition of a\n' +
+            'residential building is always a great challenge',
+        'imgName': 'OvreBuilding.png',
+    }
+]
