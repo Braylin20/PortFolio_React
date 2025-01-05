ec0d8703aae658ebf7d969df9677dfed325b81b6
Braylin20 <braylin_vasquez@ucne.edu.do>
2025-01-05 15:43:56 -0400

    Add new route

--- portfolio-michelle/src/router/AppRouter.jsx
+++ portfolio-michelle/src/router/AppRouter.jsx
@@ -4,6 +4,7 @@ import { MainSection} from "../components/MainSection.jsx";
 import {Contact} from "../components/Contact.jsx";
 import {AboutMe} from "../components/AboutMe.jsx";
 import {Projects} from "../components/Projects.jsx";
+import {ProjectPresentation} from "../components/ProjectPresentation.jsx";
 
 
 export const AppRouter = () => {
@@ -14,6 +15,7 @@ export const AppRouter = () => {
                   <Route path="/" element={<AboutMe />} />
                   <Route path="contact" element={<Contact />} />
                   <Route path="projects" element={<Projects/>} />
+                  <Route path="projects/presentation" element={<ProjectPresentation/>} />
                   <Route path="skills" element={<div>Habilidades</div>} />
                   <Route path="experience" element={<div>Experiencia laboral</div>} />
               </Route>
