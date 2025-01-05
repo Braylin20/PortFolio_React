7c6ba0bcba864ea0fdad316388025d893762818c
Braylin20 <braylin_vasquez@ucne.edu.do>
2025-01-05 14:13:47 -0400

    aAdd component Projects into the Route

--- portfolio-michelle/src/router/AppRouter.jsx
+++ portfolio-michelle/src/router/AppRouter.jsx
@@ -3,6 +3,7 @@ import {Route, Routes} from "react-router";
 import { MainSection} from "../components/MainSection.jsx";
 import {Contact} from "../components/Contact.jsx";
 import {AboutMe} from "../components/AboutMe.jsx";
+import {Projects} from "../components/Projects.jsx";
 
 
 export const AppRouter = () => {
@@ -12,7 +13,7 @@ export const AppRouter = () => {
               <Route path="/" element={<MainSection />}>
                   <Route path="/" element={<AboutMe />} />
                   <Route path="contact" element={<Contact />} />
-                  <Route path="projects" element={<div>Proyectos</div>} />
+                  <Route path="projects" element={<Projects/>} />
                   <Route path="skills" element={<div>Habilidades</div>} />
                   <Route path="experience" element={<div>Experiencia laboral</div>} />
               </Route>
