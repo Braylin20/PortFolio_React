f644c3d1b8881415f506aebbfb4d5f7e61095554
Braylin20 <braylin_vasquez@ucne.edu.do>
2025-01-09 16:35:01 -0400

    Add a new project to show it

--- portfolio-michelle/src/data/Projects.js
+++ portfolio-michelle/src/data/Projects.js
@@ -18,5 +18,11 @@ export const dataProjects =  [
         'description': 'The integration of balconies in the architectural composition of a\n' +
             'residential building is always a great challenge',
         'imgName': 'OvreBuilding.png',
+    },
+    {
+        'projectName': 'OvreBuilding',
+        'description': 'The integration of balconies in the architectural composition of a\n' +
+            'residential building is always a great challenge',
+        'imgName': 'OvreBuilding.png',
     }
 ]
