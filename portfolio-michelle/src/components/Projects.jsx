98f53bde9398ac07ace559390bfc621ed2d8470e
Braylin20 <braylin_vasquez@ucne.edu.do>
2025-01-09 16:35:25 -0400

    Involve h2 and div into a new div

--- portfolio-michelle/src/components/Projects.jsx
+++ portfolio-michelle/src/components/Projects.jsx
@@ -7,19 +7,22 @@ export const Projects = () => {
 
     return (
         <>
-            <h2 className="text-3xl mb-8 relative w-fit">
-                Projects
-                <div
-                    className="absolute bottom-[-10px] left-0 w-full h-1 bg-gradient-to-r from-blue-400 to-blue-600 rounded-full"></div>
-            </h2>
-            <div className="grid grid-cols-3 gap-4">
-                {dataProjects.map(project => (
-                    <ProjectCard
-                        key={project.projectName}
-                        {...project}
-                    />
-                ))}
+            <div className="h-screen">
+                <h2 className="text-3xl mb-8 relative w-fit">
+                    Projects
+                    <div
+                        className="absolute bottom-[-10px] left-0 w-full h-1 bg-gradient-to-r from-blue-400 to-blue-600 rounded-full"></div>
+                </h2>
+                <div className="grid grid-cols-3 gap-4">
+                    {dataProjects.map(project => (
+                        <ProjectCard
+                            key={project.projectName}
+                            {...project}
+                        />
+                    ))}
+                </div>
             </div>
+
 
         </>
 
