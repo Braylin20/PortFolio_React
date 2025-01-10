858f91d03a9efa4485677fbba3f9c3e4c7080b8b
Braylin20 <braylin_vasquez@ucne.edu.do>
2025-01-09 20:12:23 -0400

    Edit how to see the card

--- portfolio-michelle/src/components/ProjectCard.jsx
+++ portfolio-michelle/src/components/ProjectCard.jsx
@@ -1,20 +1,20 @@
-import {NavLink} from "react-router";
+import { NavLink } from "react-router";
 
-
-export const ProjectCard = ({description, imgName, projectName}) => {
-    return(
-        <>
-            <NavLink to="presentation" className="max-w-72 bg-white border border-gray-200 rounded-lg shadow">
-
-                <img className="rounded-t-lg" src={`/public/img/${imgName}`} alt=""/>
-
-                <div className="px-3 my-4">
-                    <h4 className="font-bold text-lg">{projectName}</h4>
-                    <p>
-                        {description}
-                    </p>
-                </div>
-            </NavLink>
-        </>
-    )
-}
+export const ProjectCard = ({ description, imgName, projectName }) => {
+    return (
+        <NavLink
+            to={`presentation/${projectName}`}
+            className="max-w-72 bg-white border border-gray-200 rounded-lg shadow-lg transition-transform duration-300 hover:scale-105 hover:shadow-2xl"
+        >
+            <img
+                className="rounded-t-lg w-full object-cover"
+                src={`/public/img/${imgName}`}
+                alt={projectName}
+            />
+            <div className="px-3 my-4">
+                <h4 className="font-bold text-lg">{projectName}</h4>
+                <p>{description}</p>
+            </div>
+        </NavLink>
+    );
+};
