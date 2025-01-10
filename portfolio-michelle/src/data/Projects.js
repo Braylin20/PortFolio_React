e1659371921477d98f535322ed689f82413ff4b5
Braylin20 <braylin_vasquez@ucne.edu.do>
2025-01-09 20:12:52 -0400

    Add some images to present the project

--- portfolio-michelle/src/data/Projects.js
+++ portfolio-michelle/src/data/Projects.js
@@ -6,21 +6,29 @@ export const dataProjects =  [
         'description': 'The integration of balconies in the architectural composition of a\n' +
             'residential building is always a great challenge',
         'imgName': 'OvreBuilding.png',
+        imagesPresentation: [
+            'img1.png',
+            'img2.png',
+            'img3.png',
+            'img4.png',
+            'img7.png',
+            'img8.png',
+        ]
     },
     {
-        'projectName': 'OvreBuilding',
+        'projectName': 'OvreBuilding2',
         'description': 'The integration of balconies in the architectural composition of a\n' +
             'residential building is always a great challenge',
         'imgName': 'OvreBuilding.png',
     },
     {
-        'projectName': 'OvreBuilding',
+        'projectName': 'OvreBuilding3',
         'description': 'The integration of balconies in the architectural composition of a\n' +
             'residential building is always a great challenge',
         'imgName': 'OvreBuilding.png',
     },
     {
-        'projectName': 'OvreBuilding',
+        'projectName': 'OvreBuilding4',
         'description': 'The integration of balconies in the architectural composition of a\n' +
             'residential building is always a great challenge',
         'imgName': 'OvreBuilding.png',
