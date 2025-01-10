c2f26cc8aa5929a1eb5da1bffa14e73359d361fe
Braylin20 <braylin_vasquez@ucne.edu.do>
2025-01-09 20:11:06 -0400

    Add a dynamic route

--- portfolio-michelle/src/router/AppRouter.jsx
+++ portfolio-michelle/src/router/AppRouter.jsx
@@ -1,6 +1,6 @@
 import {Route, Routes} from "react-router";
 
-import { MainSection} from "../components/MainSection.jsx";
+import {MainSection} from "../components/MainSection.jsx";
 import {Contact} from "../components/Contact.jsx";
 import {AboutMe} from "../components/AboutMe.jsx";
 import {Projects} from "../components/Projects.jsx";
@@ -8,18 +8,16 @@ import {ProjectPresentation} from "../components/ProjectPresentation.jsx";
 
 
 export const AppRouter = () => {
-  return (
-      <>
-          <Routes>
-              <Route path="/" element={<MainSection />}>
-                  <Route path="/" element={<AboutMe />} />
-                  <Route path="contact" element={<Contact />} />
-                  <Route path="projects" element={<Projects/>} />
-                  <Route path="projects/presentation" element={<ProjectPresentation/>} />
-                  <Route path="skills" element={<div>Habilidades</div>} />
-                  <Route path="experience" element={<div>Experiencia laboral</div>} />
-              </Route>
-          </Routes>
-      </>
-  )
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<MainSection/>}/>
+                <Route path="contact" element={<Contact/>}/>
+                <Route path="projects" element={<Projects/>}/>
+                <Route path="presentation/:projectName" element={<ProjectPresentation />} />
+                <Route path="skills" element={<div>Habilidades</div>}/>
+                <Route path="experience" element={<div>Experiencia laboral</div>}/>
+            </Routes>
+        </>
+    )
 }
